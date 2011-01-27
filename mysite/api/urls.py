b4027566c78678f0cf4b026f6ba5af14ac3f24c8
Zhe <linuxcity.jn@gmail.com>
2011-01-27 22:22:51 +0100

    some test

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from mysite.api.handlers import BookHandler
+from mysite.api.handlers import BookHandler, AuthorHandler
 
 book_handler = Resource(BookHandler)
+author_handler = Resource(AuthorHandler)
 
 urlpatterns = patterns('',
         url(r'^book/(?P<book_id>[^/]+)/', book_handler),
         url(r'^books/', book_handler),
+        url(r'^author/', author_handler),
     )
