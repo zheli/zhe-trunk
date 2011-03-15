0101df4813b5170413ac67ab2d33a5cbe55281d8
Zhe <linuxcity.jn@gmail.com>
2011-03-15 16:20:22 +0100

    try to use the sample code

--- mysite/upload/views.py
+++ mysite/upload/views.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers       import reverse
 from django.http                    import HttpResponseRedirect
+from django.shortcuts               import get_object_or_404
 from django.views.generic.simple    import direct_to_template
-from filetransfers.api              import prepare_upload
-from models                         import UploadForm
+from filetransfers.api              import prepare_upload, serve_file
+from models                         import UploadForm, UploadModel
 
 def upload_handler(request):
     view_url = reverse('upload.views.upload_handler')
@@ -14,4 +15,9 @@ def upload_handler(request):
     upload_url, upload_data = prepare_upload(request, view_url)
     form = UploadForm()
     return direct_to_template(request, 'upload.html',
-            {'form': form, 'upload_url':upload_url, 'upload_data':upload_data})
+            {'form': form, 'upload_url':upload_url, 'upload_data':upload_data,
+                'uploads':UploadModel.objects.all()})
+
+def download_handler(request, pk):
+    upload = get_object_or_404(UploadModel, pk=pk)
+    return serve_file(request, upload.file, save_as=True)
