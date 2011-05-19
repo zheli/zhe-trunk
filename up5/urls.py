93694bb8c8b526e296ff9fc2b99eecf19d0073cd
Zhe <linuxcity.jn@gmail.com>
2011-05-19 20:13:51 +0000

    trying native upload model

--- up5/urls.py
+++ up5/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/',     include(admin.site.urls)),
-    url(r'',    include('up5.upload.urls')),
+    #url(r'',    include('up5.upload.urls')),
+    url(r'^test/',    include('up5.upload_test.urls')),
 )
